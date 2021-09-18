adcf6996a409fcacb75448801df9fb8a644bb712
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2021-09-17 22:13:29 -0500

    actualizacion 17/09/21

---
+++ Cibertec.Domain/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cibertec.Domain
+{
+    public class Venta
+    {
+        public int VentaId { get; set; }
+
+        public string Cliente { get; set; }
+
+        public decimal montoTotal { get; set; }
+    }
+}
